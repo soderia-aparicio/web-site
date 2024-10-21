81e5285e398a45c14a7a6ee63298cfd64a162492
soderia-aparicio <soderia.aparicio.soporte@gmail.com>
2024-10-20 23:58:07 -0300

    Solucionar problemas de migración de base de datos y restaurar funcionalidad del foro

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 # Cargar usuario según su ID
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
