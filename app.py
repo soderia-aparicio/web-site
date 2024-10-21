6250767b0a3ba5b6fdcd16e85779c8f97e4bd62b
soderia-aparicio <soderia.aparicio.soporte@gmail.com>
2024-10-21 01:44:46 -0300

    Inicializar migraciones desde cero y solucionar problemas de dependencias faltantes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash, session, g
+from flask import Flask, render_template, request, redirect, url_for, flash, session, g, send_file
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
+import pandas as pd
 
 # Cargar variables de entorno desde el archivo .env
 load_dotenv()
@@ -33,17 +34,15 @@ def load_user(user_id):
 
 # Modelo de Usuario
 class User(UserMixin, db.Model):
-    # Definición de columnas para la tabla de usuarios
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone_number = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(20), default='client')  # Campo para el rol del usuario
+    role = db.Column(db.String(20), default='client')
 
 # Modelo de Mensaje
 class Message(db.Model):
-    # Definición de columnas para la tabla de mensajes
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     message = db.Column(db.Text, nullable=False)
@@ -55,7 +54,6 @@ class Message(db.Model):
 
 # Modelo de Comentario
 class Comment(db.Model):
-    # Definición de columnas para la tabla de comentarios
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
@@ -67,7 +65,6 @@ class Comment(db.Model):
 
 # Modelo de Reparto
 class Delivery(db.Model):
-    # Definición de columnas para la tabla de repartos
     id = db.Column(db.Integer, primary_key=True)
     client_name = db.Column(db.String(200), nullable=False)
     delivery_date = db.Column(db.DateTime, nullable=False)
@@ -81,7 +78,6 @@ def role_required(roles):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
-            # Verificar si el usuario tiene el rol requerido
             if current_user.role not in roles:
                 return redirect(url_for('login'))
             return f(*args, **kwargs)
@@ -96,7 +92,6 @@ def before_request():
 # Ruta para la página principal
 @app.route('/')
 def home():
-    # Redirigir al login si el usuario no está autenticado
     if not current_user.is_authenticated:
         return redirect(url_for('login'))
     return render_template('home.html', title="Soderia Aparicio")
@@ -109,7 +104,6 @@ def news():
 # Ruta para el foro
 @app.route('/foro')
 def foro():
-    # Obtener todos los mensajes del foro, ordenados por fecha
     messages = Message.query.order_by(Message.timestamp.desc()).all()
     return render_template('foro.html', messages=messages, title="Foro")
 
@@ -118,17 +112,14 @@ def foro():
 @login_required
 def postear():
     if request.method == 'POST':
-        # Obtener los datos del formulario de publicación
         title = request.form['title']
         message_text = request.form['message']
         signature = request.form['signature']
         ip_address = request.remote_addr
         user_id = current_user.id
-        # Verificar que todos los campos estén completos
         if not title or not message_text or not signature:
             flash('Todos los campos son obligatorios')
         else:
-            # Crear un nuevo mensaje y guardarlo en la base de datos
             message = Message(title=title, message=message_text, signature=signature, ip_address=ip_address, user_id=user_id)
             db.session.add(message)
             db.session.commit()
@@ -141,11 +132,9 @@ def postear():
 @role_required(['developer', 'master'])
 def gestion_usuarios():
     if request.method == 'POST':
-        # Obtener el usuario a modificar
         user_id = request.form['user_id']
         user = User.query.get(user_id)
         if user:
-            # Actualizar rol del usuario según el rol del usuario actual
             if 'role_developer' in request.form and current_user.role == 'developer':
                 user.role = 'developer'
             elif 'role_master' in request.form and current_user.role in ['developer', 'master']:
@@ -154,7 +143,6 @@ def gestion_usuarios():
                 user.role = 'employee'
             elif 'role_client' in request.form:
                 user.role = 'client'
-            # Actualizar email y número de teléfono del usuario
             if 'email' in request.form:
                 user.email = request.form['email']
             if 'phone_number' in request.form:
@@ -167,15 +155,13 @@ def gestion_usuarios():
 # Ruta para gestionar repartos
 @app.route('/gestion_repartos', methods=['GET', 'POST'])
 @login_required
-@role_required(['developer', 'master'])
+@role_required(['developer', 'master', 'employee'])
 def gestion_repartos():
     if request.method == 'POST':
         delivery_id = request.form.get('delivery_id')
         if delivery_id:
-            # Editar entrega existente
             delivery = Delivery.query.get(delivery_id)
             if delivery:
-                # Actualizar los datos del reparto existente
                 if 'client_name' in request.form:
                     delivery.client_name = request.form['client_name']
                 if 'delivery_date' in request.form:
@@ -187,45 +173,144 @@ def gestion_repartos():
                 db.session.commit()
                 flash('Reparto actualizado exitosamente.')
         else:
-            # Crear nueva entrega
             client_name = request.form['client_name']
             delivery_date = datetime.strptime(request.form['delivery_date'], '%Y-%m-%d')
             items_delivered = request.form['items_delivered']
             notes = request.form.get('notes', '')
-            # Crear un nuevo reparto y guardarlo en la base de datos
             new_delivery = Delivery(client_name=client_name, delivery_date=delivery_date, items_delivered=items_delivered, notes=notes, user_id=current_user.id)
             db.session.add(new_delivery)
             db.session.commit()
             flash('Reparto creado exitosamente.')
-    # Obtener todos los repartos existentes
     deliveries = Delivery.query.all()
     return render_template('gestion_repartos.html', deliveries=deliveries, title="Gestión de Repartos")
 
-# Ruta para mostrar detalles de un mensaje
-@app.route('/message/<int:message_id>', methods=['GET', 'POST'])
-def message_detail(message_id):
-    # Obtener el mensaje por su ID o devolver un error 404 si no existe
-    message = Message.query.get_or_404(message_id)
-    highlighted_comment_id = None
-    if request.method == 'POST':
-        if not current_user.is_authenticated:
-            flash('You need to login to comment.', 'danger')
-            return redirect(url_for('login'))
-        text = request.form['text']
-        ip_address = request.remote_addr
-        # Crear un nuevo comentario y guardarlo en la base de datos
-        new_comment = Comment(text=text, ip_address=ip_address, user_id=current_user.id, message_id=message.id)
-        db.session.add(new_comment)
-        db.session.commit()
-        flash('Comment added successfully.', 'success')
-        highlighted_comment_id = new_comment.id
-        return redirect(url_for('message_detail', message_id=message.id, _anchor=f'comment-{highlighted_comment_id}'))
-    return render_template('message_detail.html', message=message, highlighted_comment_id=highlighted_comment_id)
+# Ruta para obtener extracto de cliente
+@app.route('/cliente/extracto/<int:client_id>', methods=['GET', 'POST'])
+@login_required
+@role_required(['developer', 'master', 'employee'])
+def obtener_extracto_cliente(client_id):
+    client = User.query.get_or_404(client_id)
+    deliveries = None
 
-# Ruta para videos
-@app.route('/videos')
-def videos():
-    return render_template('videos.html')
+    if request.method == 'POST':
+        fecha_inicio = request.form.get('fecha_inicio')
+        fecha_fin = request.form.get('fecha_fin')
+
+        if fecha_inicio and fecha_fin:
+            fecha_inicio_dt = datetime.strptime(fecha_inicio, '%Y-%m-%d')
+            fecha_fin_dt = datetime.strptime(fecha_fin, '%Y-%m-%d')
+            deliveries = Delivery.query.filter(
+                Delivery.client_name == client.username,
+                Delivery.delivery_date >= fecha_inicio_dt,
+                Delivery.delivery_date <= fecha_fin_dt
+            ).all()
+
+    return render_template('extracto_cliente.html', client=client, deliveries=deliveries)
+
+# Ruta para obtener extracto de cliente
+@app.route('/cliente/extracto/<int:client_id>', methods=['GET', 'POST'])
+@login_required
+@role_required(['developer', 'master', 'employee'])
+def obtener_extracto_cliente(client_id):
+    client = User.query.get_or_404(client_id)
+    deliveries = None
+
+    if request.method == 'POST':
+        fecha_inicio = request.form.get('fecha_inicio')
+        fecha_fin = request.form.get('fecha_fin')
+
+        if fecha_inicio and fecha_fin:
+            fecha_inicio_dt = datetime.strptime(fecha_inicio, '%Y-%m-%d')
+            fecha_fin_dt = datetime.strptime(fecha_fin, '%Y-%m-%d')
+            deliveries = Delivery.query.filter(
+                Delivery.client_name == client.username,
+                Delivery.delivery_date >= fecha_inicio_dt,
+                Delivery.delivery_date <= fecha_fin_dt
+            ).all()
+
+    return render_template('extracto_cliente.html', client=client, deliveries=deliveries)
+
+# Ruta para descargar extracto de cliente en Excel
+@app.route('/cliente/extracto/descargar/<int:client_id>', methods=['POST'])
+@login_required
+@role_required(['developer', 'master', 'employee'])
+def descargar_extracto_cliente(client_id):
+    client = User.query.get_or_404(client_id)
+    fecha_inicio = request.form.get('fecha_inicio')
+    fecha_fin = request.form.get('fecha_fin')
+
+    if not fecha_inicio or not fecha_fin:
+        flash('Por favor ingrese ambas fechas.', 'danger')
+        return redirect(url_for('obtener_extracto_cliente', client_id=client.id))
+
+    # Convertir las fechas a objetos datetime
+    fecha_inicio_dt = datetime.strptime(fecha_inicio, '%Y-%m-%d')
+    fecha_fin_dt = datetime.strptime(fecha_fin, '%Y-%m-%d')
+
+    # Obtener los repartos del cliente en el rango de fechas
+    deliveries = Delivery.query.filter(
+        Delivery.client_name == client.username,
+        Delivery.delivery_date >= fecha_inicio_dt,
+        Delivery.delivery_date <= fecha_fin_dt
+    ).all()
+
+    # Crear un DataFrame con los datos obtenidos
+    data = [{
+        'Fecha de Entrega': delivery.delivery_date.strftime('%Y-%m-%d'),
+        'Ítems Entregados': delivery.items_delivered,
+        'Notas': delivery.notes
+    } for delivery in deliveries]
+
+    # Verificar si hay datos para exportar
+    if not data:
+        flash('No hay repartos disponibles en el rango de fechas seleccionado.', 'warning')
+        return redirect(url_for('obtener_extracto_cliente', client_id=client.id))
+
+    # Crear un DataFrame con los datos
+    df = pd.DataFrame(data)
+
+    # Guardar el DataFrame en un archivo Excel temporal
+    excel_path = f"/tmp/extracto_cliente_{client.username}_{fecha_inicio}_{fecha_fin}.xlsx"
+    df.to_excel(excel_path, index=False)
+
+    # Enviar el archivo al cliente para su descarga
+    return send_file(excel_path, as_attachment=True, download_name=f"Extracto_{client.username}_{fecha_inicio}_{fecha_fin}.xlsx")
+
+
+# Ruta para descargar extracto de cliente
+@app.route('/cliente/extracto/descargar/<int:client_id>', methods=['POST'])
+@login_required
+@role_required(['developer', 'master', 'employee'])
+def descargar_extracto_cliente(client_id):
+    client = User.query.get_or_404(client_id)
+    fecha_inicio = request.form.get('fecha_inicio')
+    fecha_fin = request.form.get('fecha_fin')
+
+    if not fecha_inicio or not fecha_fin:
+        flash('Por favor ingrese ambas fechas.', 'danger')
+        return redirect(url_for('obtener_extracto_cliente', client_id=client.id))
+
+    fecha_inicio_dt = datetime.strptime(fecha_inicio, '%Y-%m-%d')
+    fecha_fin_dt = datetime.strptime(fecha_fin, '%Y-%m-%d')
+    deliveries = Delivery.query.filter(
+        Delivery.client_name == client.username,
+        Delivery.delivery_date >= fecha_inicio_dt,
+        Delivery.delivery_date <= fecha_fin_dt
+    ).all()
+
+    # Crear un dataframe con las entregas
+    data = [{
+        'Fecha de entrega': delivery.delivery_date,
+        'Cliente': delivery.client_name,
+        'Artículos entregados': delivery.items_delivered,
+        'Notas': delivery.notes
+    } for delivery in deliveries]
+
+    df = pd.DataFrame(data)
+    excel_path = f'/tmp/extracto_cliente_{client.username}.xlsx'
+    df.to_excel(excel_path, index=False)
+
+    return send_file(excel_path, as_attachment=True)
 
 # Ruta para iniciar sesión
 @app.route('/login', methods=['GET', 'POST'])
@@ -234,10 +319,9 @@ def login():
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
-        # Verificar la contraseña del usuario
         if user and check_password_hash(user.password, password):
             login_user(user)
-            return redirect(url_for('foro'))
+            return redirect(url_for('home'))
         flash('Nombre de usuario o contraseña incorrectos')
     return render_template('login.html', title="Login")
 
@@ -249,7 +333,6 @@ def logout():
     return redirect(url_for('home'))
 
 # Ruta para registrar un nuevo usuario
-# Ruta para registrar un nuevo usuario
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -259,20 +342,16 @@ def register():
         password = request.form['password']
         confirm_password = request.form['confirm_password']
 
-        # Verificar si las contraseñas coinciden
         if password != confirm_password:
             flash('Passwords do not match. Please try again.', 'danger')
             return redirect(url_for('register'))
 
-        # Crear un nuevo usuario con los datos ingresados
         hashed_password = generate_password_hash(password, method='sha256')
         new_user = User(username=username, email=email, phone_number=phone_number, password=hashed_password, role='client')
 
-        # Agregar el nuevo usuario a la base de datos
         db.session.add(new_user)
         db.session.commit()
 
-        # Si el usuario es del tipo empleado, iniciar sesión automáticamente
         if new_user.role == 'employee':
             login_user(new_user)
             flash('Registro exitoso y has sido logueado automáticamente.', 'success')
@@ -283,12 +362,6 @@ def register():
 
     return render_template('register.html', title="Register")
 
-# Ruta para el perfil del usuario
-@app.route('/perfil')
-@login_required
-def perfil():
-    return render_template('perfil.html', user=current_user, title="Perfil del Usuario")
-
 # Ruta para editar el perfil del usuario
 @app.route('/perfil/editar', methods=['GET', 'POST'])
 @login_required
@@ -298,7 +371,6 @@ def editar_perfil():
         current_user.email = request.form['email']
         current_user.phone_number = request.form['phone_number']
 
-        # Guardar los cambios en la base de datos
         db.session.commit()
         flash('Perfil actualizado exitosamente.', 'success')
         return redirect(url_for('perfil'))
